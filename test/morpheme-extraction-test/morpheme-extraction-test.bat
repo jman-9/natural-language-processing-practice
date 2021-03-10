f4c6cf1cbfc3a548f4730ccd0963a8d6f0e6d1a7
jman-9 <flute999@daum.net>
2021-03-11 02:39:35 +0900

    upload morpheme-extraction-test

---
+++ test/morpheme-extraction-test/morpheme-extraction-test.bat
@@ -0,0 +1,5 @@
+set MECAB=mecab-ko\mecab.exe -r mecab-ko\mecabrc -d mecab-ko\mecab-ko-dic\
+
+%MECAB% rawdata\economy.txt -o utf8-econ.txt
+%MECAB% rawdata\humanities.txt -o utf8-human.txt
+%MECAB% rawdata\science.txt -o utf8-sci.txt
---
+++ test/morpheme-extraction-test/morpheme-extraction-test.cpp
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	system("morpheme-extraction-test.bat");
+}
